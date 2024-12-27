b0207a3f8a3024cd4993ef41c270d2e5ae3efe87
huzehui <huzehui@starcdata.com>
2024-12-27 17:41:43 +0800

    新增local.strategy

---
+++ src/user/local/local.strategy.ts
@@ -0,0 +1,32 @@
+import { BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { compareSync } from 'bcryptjs';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, IStrategyOptions } from 'passport-local';
+import { User } from '../entities/user.entity';
+
+export class LocalStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) {
+        super({
+            usernameField: 'username',
+            passwordField: 'password',
+        } as IStrategyOptions);
+    }
+    async validate(username: string, password: string): Promise<User> {
+        const user = await this.userRepository.createQueryBuilder('user')
+            .addSelect('user.password')
+            .where('user.username = :username', { username })
+            .getOne();
+        if (!user) {    
+            throw new BadRequestException('用户不存在');
+        }
+        if (!compareSync(password, user.password)) {
+            throw new BadRequestException('密码错误');
+        }
+        return user;
+    }
+}
